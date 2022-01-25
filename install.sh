87e281c57410b652d497f9b8d5057f568944da69
christopher-besch <christopher.besch@gmx.de>
2022-01-25 09:18:57 +0100

    only source .bashrc once; added .inputrc

--- install.sh
+++ install.sh
@@ -3,19 +3,23 @@ set -euo pipefail
 IFS=$' \n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-rm ~/.clang-format || true
-ln -s $DIR/.clang-format ~/.clang-format
+rm -v ~/.clang-format || true
+ln -vs $DIR/.clang-format ~/.clang-format
 
-rm ~/.gdbinit || true
-ln -s $DIR/.gdbinit ~/.gdbinit
+rm -v ~/.gdbinit || true
+ln -vs $DIR/.gdbinit ~/.gdbinit
 
-rm ~/.gitconfig || true
-ln -s $DIR/.gitconfig ~/.gitconfig
+rm -v ~/.gitconfig || true
+ln -vs $DIR/.gitconfig ~/.gitconfig
 
-rm ~/.indentconfig.yaml || true
+rm -v ~/.inputrc || true
+ln -vs $DIR/.inputrc ~/.inputrc
+
+rm -v ~/.indentconfig.yaml || true
 cat <<EOF > ~/.indentconfig.yaml
 paths:
   - $DIR/latexindent_config.yaml
 EOF
 
-echo "source $DIR/.bashrc" >> ~/.bashrc
+# only append sourcing when not already done
+grep -E 'source .+/\.bashrc$' ~/.bashrc > /dev/null || echo "source $DIR/.bashrc" >> ~/.bashrc
