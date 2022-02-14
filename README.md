0e120697998481c0806382ef7a8a59f6a34f191b
christopher-besch <christopher.besch@gmx.de>
2022-02-14 20:21:22 +0000

    added kitty config; fixed xfce config; better git config

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # How to Cook Like I Do?
 
-- clone this repo and run `./install.sh`
-- install [my Kitty config](https://github.com/christopher-besch/kitty_like_me)
+- clone this repo recursively and run `./install.sh`
 - install [my LunarVim config](https://github.com/christopher-besch/lvim_like_me)
 
 # Resources
--- install.sh
+++ install.sh
@@ -3,26 +3,49 @@ set -euo pipefail
 IFS=$' \n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-rm -v ~/.clang-format || true
-ln -vs $DIR/.clang-format ~/.clang-format
+echo installing clang-format config
+ln -fvs $DIR/.clang-format ~/.clang-format
 
-rm -v ~/.gdbinit || true
-ln -vs $DIR/.gdbinit ~/.gdbinit
+echo installing gdb config
+ln -fvs $DIR/.gdbinit ~/.gdbinit
 
-rm -v ~/.gitconfig || true
-ln -vs $DIR/.gitconfig ~/.gitconfig
+echo installing git config
+grep -E ' *path = .+/\gitconfig$' ~/.gitconfig > /dev/null || printf "[include]\n path = $DIR/gitconfig\n" >> ~/.gitconfig
 
-rm -v ~/.inputrc || true
-ln -vs $DIR/.inputrc ~/.inputrc
+echo installing inputrc config
+ln -fvs $DIR/.inputrc ~/.inputrc
 
-rm -v ~/.Xmodmap || true
-ln -vs $DIR/.Xmodmap ~/.Xmodmap
+echo installing xfce config
+ln -fvs $DIR/.Xmodmap ~/.Xmodmap
+ln -fvs $DIR/user-dirs.dirs ~/.config/user-dirs.dirs
+# create dump and dwn dirs
+mkdir -vp ~/.dump
+mkdir -vp ~/dwn
 
-rm -v ~/.indentconfig.yaml || true
+echo installing indentconfig config
 cat <<EOF > ~/.indentconfig.yaml
 paths:
   - $DIR/latexindent_config.yaml
 EOF
 
+echo installing bashrc
 # only append sourcing when not already done
-grep -E 'source .+/\.bashrc$' ~/.bashrc > /dev/null || echo "source $DIR/.bashrc" >> ~/.bashrc
+grep -E 'source .+/bashrc$' ~/.bashrc > /dev/null || echo "source $DIR/bashrc" >> ~/.bashrc
+
+echo installing font
+curl -o font.zip -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+rm -r $HOME/.local/share/fonts || true
+mkdir -vp $HOME/.local/share/fonts
+mv -v font.zip $HOME/.local/share/fonts
+cd $HOME/.local/share/fonts
+unzip font.zip
+rm -v font.zip
+fc-cache -f -v
+
+echo installing kitty config
+rm -rvf ~/.config/kitty
+mkdir -vp ~/.config/kitty
+git clone https://github.com/dexpota/kitty-themes ~/.config/kitty
+ln -fvs $DIR/kitty.conf ~/.config/kitty/kitty.conf
+
+echo "All done! Have a nice day."
