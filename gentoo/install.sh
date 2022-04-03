abe37b0712ae801bcf456c79432b9387f2c3b76c
christopher-besch <mail@chris-besch.com>
2022-04-03 09:36:39 +0200

    disabled pc speaker

--- gentoo/install.sh
+++ gentoo/install.sh
@@ -3,17 +3,20 @@ set -euo pipefail
 IFS=$' \n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-echo installing /etc/default/grub
+echo "installing /etc/default/grub"
 ln -fvs $DIR/etc_default_grub /etc/default/grub
 
-echo installing /usr/share/sddm/scripts/Xsetup
+echo "installing /usr/share/sddm/scripts/Xsetup"
 ln -fvs $DIR/usr_share_sddm_scripts_Xsetup /usr/share/sddm/scripts/Xsetup
 
-echo installing /etc/portage/make.conf
+echo "installing /etc/portage/make.conf"
 ln -fvs $DIR/etc_portage_make.conf /etc/portage/make.conf
 
-echo installing /etc/portage/package.use
+echo "installing /etc/portage/package.use"
 ln -fvs $DIR/etc_portage_package.use /etc/portage/package.use
+
+echo "installing /etc/modprobe.d/blacklist.conf"
+ln -fvs $DIR/etc_modprobe.d_blacklist.conf /etc/modprobe.d/blacklist.conf
 
 echo "All done! Have a nice day."
 
--- install.sh
+++ install.sh
@@ -3,40 +3,40 @@ set -euo pipefail
 IFS=$' \n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-echo installing clang-format config
+echo "installing clang-format config"
 ln -fvs $DIR/.clang-format ~/.clang-format
 
-echo installing GDB config
+echo "installing GDB config"
 ln -fvs $DIR/.gdbinit ~/.gdbinit
 
-echo installing Git config
+echo "installing Git config"
 # only append include when not already done
 grep -E ' *path = .+/\gitconfig$' ~/.gitconfig > /dev/null || printf "[include]\n path = $DIR/gitconfig\n" >> ~/.gitconfig
 
-echo installing gpg config
+echo "installing gpg config"
 ln -fvs $DIR/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 
-echo installing inputrc config
+echo "installing inputrc config"
 ln -fvs $DIR/.inputrc ~/.inputrc
 
-echo installing Xfce config
+echo "installing Xfce config"
 ln -fvs $DIR/.Xmodmap ~/.Xmodmap
 ln -fvs $DIR/user-dirs.dirs ~/.config/user-dirs.dirs
 # create dump and dwn dirs
 mkdir -vp ~/.dump
 mkdir -vp ~/dwn
 
-echo installing latexindent config
+echo "installing latexindent config"
 cat <<EOF > ~/.indentconfig.yaml
 paths:
   - $DIR/latexindent_config.yaml
 EOF
 
-echo installing bashrc
+echo "installing bashrc"
 # only append sourcing when not already done
 grep -E 'source .+/bashrc$' ~/.bashrc > /dev/null || echo "source $DIR/bashrc" >> ~/.bashrc
 
-echo installing font
+echo "installing font"
 curl -o font.zip -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
 rm -r $HOME/.local/share/fonts || true
 mkdir -vp $HOME/.local/share/fonts
@@ -46,17 +46,17 @@ unzip font.zip
 rm -v font.zip
 fc-cache -f -v
 
-echo installing kitty config
+echo "installing kitty config"
 rm -rvf ~/.config/kitty
 mkdir -vp ~/.config/kitty
 git clone https://github.com/dexpota/kitty-themes ~/.config/kitty
 ln -fvs $DIR/kitty.conf ~/.config/kitty/kitty.conf
 
-echo installing LunarVim config
+echo "installing LunarVim config"
 mkdir -vp ~/.config/lvim
 ln -fvs $DIR/config.lua ~/.config/lvim/config.lua
 
-echo installing LunarVim German Spelling
+echo "installing LunarVim German Spelling"
 # fixes a bug: https://github.com/LunarVim/LunarVim/issues/1445
 mkdir -p ~/.local/share/lunarvim/site/spell
 ln -fvs $DIR/vendor/de.utf-8.spl ~/.local/share/lunarvim/site/spell/de.utf-8.spl
