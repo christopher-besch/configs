1ad56c58ac942e4b81a7c4ebdfde6b9426b761b0
christopher-besch <mail@chris-besch.com>
2022-08-08 11:39:53 +0200

    better install.sh messages

--- install.sh
+++ install.sh
@@ -26,12 +26,15 @@ echo "### installing chris' config for type: $INSTALL_TYPE ###"
 # both desktop and server #
 ###########################
 
+echo
 echo "installing GDB config"
 ln -fvs $DIR/.gdbinit ~/.gdbinit
 
+echo
 echo "installing inputrc config"
 ln -fvs $DIR/.inputrc ~/.inputrc
 
+echo
 echo "installing bashrc"
 # only append sourcing when not already done
 grep -E '^source .+/bashrc_(desktop|server)$' ~/.bashrc || echo "source $DIR/bashrc_$INSTALL_TYPE" >> ~/.bashrc
@@ -41,17 +44,21 @@ grep -E '^source .+/bashrc_(desktop|server)$' ~/.bashrc || echo "source $DIR/bas
 ########################
 
 if [[ $INSTALL_TYPE == "desktop" ]]; then
+    echo
     echo "installing clang-format config"
     ln -fvs $DIR/.clang-format ~/.clang-format
 
+    echo
     echo "installing Git config"
     # only append include when not already done
     grep -E ' *path = .+/\gitconfig$' ~/.gitconfig > /dev/null || printf "[include]\n path = $DIR/gitconfig\n" >> ~/.gitconfig
 
+    echo
     echo "installing gpg config"
     mkdir -p ~/.gnupg
     ln -fvs $DIR/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 
+    echo
     echo "installing Xfce config"
     mkdir -p ~/.config
     ln -fvs $DIR/user-dirs.dirs ~/.config/user-dirs.dirs
@@ -59,12 +66,14 @@ if [[ $INSTALL_TYPE == "desktop" ]]; then
     mkdir -vp ~/.dump
     mkdir -vp ~/dwn
 
+    echo
     echo "installing latexindent config"
     cat <<EOF > ~/.indentconfig.yaml
 paths:
   - $DIR/latexindent_config.yaml
 EOF
 
+    echo
     echo "installing font"
     curl -o font.zip -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
     rm -r $HOME/.local/share/fonts || true
@@ -75,27 +84,32 @@ EOF
     rm -v font.zip
     fc-cache -f -v
 
+    echo
     echo "installing kitty config"
     rm -rvf ~/.config/kitty
     mkdir -vp ~/.config/kitty
     git clone https://github.com/dexpota/kitty-themes ~/.config/kitty
     ln -fvs $DIR/kitty.conf ~/.config/kitty/kitty.conf
 
+    echo
     echo "installing LunarVim config"
     mkdir -vp ~/.config/lvim
     ln -fvs $DIR/config.lua ~/.config/lvim/config.lua
 
+    echo
     echo "installing LunarVim German Spelling"
     # fixes a bug: https://github.com/LunarVim/LunarVim/issues/1445
     mkdir -p ~/.local/share/lunarvim/site/spell
     ln -fvs $DIR/vendor/de.utf-8.spl ~/.local/share/lunarvim/site/spell/de.utf-8.spl
     ln -fvs $DIR/vendor/de.utf-8.sug ~/.local/share/lunarvim/site/spell/de.utf-8.sug
 
+    echo
     echo "installing custom keyboard layout; please enter sudo password"
     sudo ln -fvs $DIR/chris_keyboard /usr/share/X11/xkb/symbols/chris_keyboard
     setxkbmap chris_keyboard
 fi
 
+echo
 echo "All done! Have a nice day."
 exit 0
 
