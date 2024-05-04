c90f1d172628282ed5c356414e9b75b7dba6cd2d
christopher-besch <mail@chris-besch.com>
2024-05-04 20:15:04 +0200

    added git lg command alias

--- install.sh
+++ install.sh
@@ -125,6 +125,17 @@ EOF
 [init]
     defaultBranch = main
 
+# copied from: https://stackoverflow.com/questions/1838873/visualizing-branch-topology-in-git/34467298#34467298
+[alias]
+    lg = lg1
+    lg1 = lg1-specific --all
+    lg2 = lg2-specific --all
+    lg3 = lg3-specific --all
+
+    lg1-specific = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'
+    lg2-specific = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'
+    lg3-specific = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'
+
 # configs for different remotes
 [includeIf "hasconfig:remote.*.url:git@github.com:*/**"]
     path = $DIR/gitconfig_private
